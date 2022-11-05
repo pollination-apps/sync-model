24dd4ab95f8abf2bb0801c4117488d0f07914883
Chris Mackey <chris@ladybug.tools>
2022-11-05 12:40:26 -0400

    fix(app): Add sample to show ComparisonReport

--- app/app.py
+++ app/app.py
@@ -1,3 +1,13 @@
+import os
 import streamlit as st
+from honeybee.model import Model
 
 st.write("Welcome! Start writing your Pollination app here.")
+
+
+model_1_path = '{}/samples/existing_model.hbjson'.format(os.path.dirname(__file__))
+model_2_path = '{}/samples/updated_model.hbjson'.format(os.path.dirname(__file__))
+model_1 = Model.from_hbjson(model_1_path)
+model_2 = Model.from_hbjson(model_2_path)
+
+st.json(model_1.comparison_report(model_2))
